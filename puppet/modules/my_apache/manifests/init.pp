90842c836cd5cc2762955073d9bfc6ee5546b6ae
Jim <James.Domino@dell.com>
2017-12-03 23:56:51 -0500

    touching to trigger abuild   puppet/modules/my_apache/manifests/init.pp

--- puppet/modules/my_apache/manifests/init.pp
+++ puppet/modules/my_apache/manifests/init.pp
@@ -73,6 +73,7 @@ exec { 'apache_run':
 
 #Exec['install_Docker'] -> File['apache2']
 #Exec['install_Docker'] -> File[$docker_root]
+#
 File['/var/src'] -> File[$docker_root] 
 File[$docker_root] -> File['apache2']
 File['apache2'] -> Exec['apache_build']
